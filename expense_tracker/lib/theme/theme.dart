e07c2317521908546098f03f43bcc573c1892109
lonewanderer27 <adrianejames27@gmail.com>
2025-06-16 11:20:04 +0800

    fix(expense_tracker): card theme data type mismatch

--- expense_tracker/lib/theme/theme.dart
+++ expense_tracker/lib/theme/theme.dart
@@ -3,25 +3,15 @@ import 'package:flutter/material.dart';
 
 var theme = ThemeData().copyWith(
     colorScheme: kColorScheme,
-    appBarTheme: AppBarTheme().copyWith(
-        backgroundColor: kColorScheme.onPrimaryContainer,
-        foregroundColor: kColorScheme.primaryContainer),
-    cardTheme: const CardTheme().copyWith(
-        color: kColorScheme.secondaryContainer,
-        margin: const EdgeInsets.symmetric(horizontal: 16, vertical: 8)),
-    elevatedButtonTheme: ElevatedButtonThemeData(
-        style: ElevatedButton.styleFrom(
-            backgroundColor: kColorScheme.primaryContainer)));
+    appBarTheme: AppBarTheme().copyWith(backgroundColor: kColorScheme.onPrimaryContainer, foregroundColor: kColorScheme.primaryContainer),
+    cardTheme:
+        const CardTheme().copyWith(color: kColorScheme.secondaryContainer, margin: const EdgeInsets.symmetric(horizontal: 16, vertical: 8)).data,
+    elevatedButtonTheme: ElevatedButtonThemeData(style: ElevatedButton.styleFrom(backgroundColor: kColorScheme.primaryContainer)));
 
 var darkTheme = ThemeData.dark().copyWith(
     colorScheme: kDarkColorScheme,
-    appBarTheme: AppBarTheme().copyWith(
-        backgroundColor: kColorScheme.onPrimaryContainer,
-        foregroundColor: kColorScheme.primaryContainer),
-    cardTheme: const CardTheme().copyWith(
-        color: kColorScheme.secondaryContainer,
-        margin: const EdgeInsets.symmetric(horizontal: 16, vertical: 8)),
+    appBarTheme: AppBarTheme().copyWith(backgroundColor: kColorScheme.onPrimaryContainer, foregroundColor: kColorScheme.primaryContainer),
+    cardTheme:
+        const CardTheme().copyWith(color: kColorScheme.secondaryContainer, margin: const EdgeInsets.symmetric(horizontal: 16, vertical: 8)).data,
     elevatedButtonTheme: ElevatedButtonThemeData(
-        style: ElevatedButton.styleFrom(
-            backgroundColor: kDarkColorScheme.primaryContainer,
-            foregroundColor: kDarkColorScheme.secondaryContainer)));
+        style: ElevatedButton.styleFrom(backgroundColor: kDarkColorScheme.primaryContainer, foregroundColor: kDarkColorScheme.secondaryContainer)));
